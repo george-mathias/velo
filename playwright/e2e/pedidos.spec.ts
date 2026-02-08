d2a4aea8be43de4c4b0dbfcd2b42a9d848c843a6
George Mathias <gmathias80@gmail.com>
2026-02-08 21:09:38 +0000

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,18 +1,21 @@
 import { test, expect } from '@playwright/test'
 
-test('deve consultar um pedido', async ({ page }) => {
-  await page.goto('http://localhost:5173/')
+/// AAA - Arrange, Act, Assert
 
+test('deve consultar um pedido', async ({ page }) => {
+  
+  // Arrange
+  await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-IJJFFY')
-
   await page.getByTestId('search-order-button').click()
 
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-IJJFFY')
   
@@ -20,9 +23,3 @@ test('deve consultar um pedido', async ({ page }) => {
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 
 })
-
-
-
-
-
-
