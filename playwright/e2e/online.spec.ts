871d18b6a1cf5a1db446612c9e00e15aade71dac
George Mathias <gmathias80@gmail.com>
2026-02-08 17:59:17 +0000

    mudando o titulo do cenario

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,8 +1,7 @@
 import { test, expect } from '@playwright/test'
 
-test('web app deve estar online', async ({ page }) => {
+test('a web app deve estar online', async ({ page }) => {
   await page.goto('http://localhost:5173/')
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Velô by Papito/)
 })
