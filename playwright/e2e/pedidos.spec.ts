253b6e61c72f2483bd89f20ac69b07c4b4413e63
George Mathias <gmathias80@gmail.com>
2026-04-22 21:13:26 +0100

    test: Papito's solution to the id challenge

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -4,7 +4,7 @@ import { test, expect } from '@playwright/test'
 
 test('deve consultar um pedido', async ({ page }) => {
 
-  const orderNumber = 'VLO-IJJFFY'
+  const orderNumber = "VLO-IJJFFY"
 
   // Arrange
   await page.goto('http://localhost:5173/')
@@ -17,14 +17,26 @@ test('deve consultar um pedido', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(orderNumber)
   await page.getByRole('button', { name: 'Buscar Pedido'}).click()
 
-  // Assert  
-  await expect(page.getByTestId(`order-result-${orderNumber}`)).toBeVisible()
+  // Assert
+  /* meu código */
+  // await expect(page.getByTestId(`order-result-${orderNumber}`)).toBeVisible()
 
-  await expect(page.getByTestId(`order-result-${orderNumber}`).locator('p', { hasText: /^Pedido$/ })).toBeVisible()
-  await expect(page.getByTestId(`order-result-${orderNumber}`).locator('p', { hasText: orderNumber })).toBeVisible()
-  await expect(page.getByTestId(`order-result-${orderNumber}`).locator('p', { hasText: orderNumber })).toContainText(orderNumber)
+  // await expect(page.getByTestId(`order-result-${orderNumber}`).locator('p', { hasText: /^Pedido$/ })).toBeVisible()
+  // await expect(page.getByTestId(`order-result-${orderNumber}`).locator('p', { hasText: orderNumber })).toBeVisible()
+  // await expect(page.getByTestId(`order-result-${orderNumber}`).locator('p', { hasText: orderNumber })).toContainText(orderNumber)
 
-  await expect(page.getByTestId(`order-result-${orderNumber}`).locator('div', { hasText: /^APROVADO$/ })).toBeVisible()
-  await expect(page.getByTestId(`order-result-${orderNumber}`).locator('div', { hasText: /^APROVADO$/ })).toContainText('APROVADO')
+  // await expect(page.getByTestId(`order-result-${orderNumber}`).locator('div', { hasText: /^APROVADO$/ })).toBeVisible()
+  // await expect(page.getByTestId(`order-result-${orderNumber}`).locator('div', { hasText: /^APROVADO$/ })).toContainText('APROVADO')
 
+  /* Código do Papito */
+  const orderCode = page.locator(`//p[text()="Pedido"]/..//p[text()="${orderNumber}"]`)
+  await expect(orderCode).toBeVisible( { timeout: 10_000 } )
+
+  const containerPedido = page.getByRole('paragraph')
+    .filter( { hasText: /^Pedido$/ } )
+    .locator('..')  // sobe um nível igual ao xpath
+
+  await expect(containerPedido).toContainText(`${orderNumber}`)
+
+  await expect(page.getByText('APROVADO')).toBeVisible()
 })
