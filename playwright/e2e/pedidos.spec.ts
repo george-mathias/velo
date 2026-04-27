5ec7a1702a1a96b1dae6da8ec223ec78b81b2ed8
George Mathias <gmathias80@gmail.com>
2026-04-27 18:50:12 +0100

    test(e2e/searchByOrderId): validate error message for invalid order number

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -40,3 +40,41 @@ test('deve consultar um pedido', async ({ page }) => {
 
   await expect(page.getByText('APROVADO')).toBeVisible()
 })
+
+test('It should return an error message when submitting an invalid order number', async ({ page }) => {
+
+  const orderNumber = "VLO-ABC123"
+  const expectedErrorMessage = "Pedido não encontrado"
+  const expectedInformationMessage = "Verifique o número do pedido e tente novamente"
+
+  // Arrange
+  await page.goto('http://localhost:5173/')
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  // Act
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(orderNumber)
+  await page.getByRole('button', { name: 'Buscar Pedido'}).click()
+
+  // Assert
+  // await expect(page.locator('#root')).toContainText('Pedido não encontrado')
+  // await expect(page.locator('#root')).toContainText('Verifique o número do pedido e tente novamente') 
+
+  // const title = page.getByRole('heading', { name: 'Pedido não encontrado', level: 3 })
+  // await expect(title).toBeVisible()
+
+  // const message = page.getByText('Verifique o número do pedido e tente novamente')
+  // const message = page.locator('//p[text()="Verifique o número do pedido e tente novamente"]')
+
+  // const message = page.locator('p', { hasText: 'Verifique o número do pedido e tente novamente' })
+  // await expect(message).toBeVisible()
+
+  await expect(page.locator('#root')).toMatchAriaSnapshot(`
+    - img
+    - heading "${expectedErrorMessage}" [level=3]
+    - paragraph: ${expectedInformationMessage}
+    `)
+
+})
