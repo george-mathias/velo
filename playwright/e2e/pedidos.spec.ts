8534997ce04447a41ddad28b68ba0dafc66f1f61
George Mathias <gmathias80@gmail.com>
2026-04-29 12:55:33 +0100

    test(e2e): including a new test

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -67,6 +67,55 @@ test.describe('View order', () => {
     `);
   })
 
+  test('It should display details for an order under review after submitting a valid order number', async ({ page }) => {
+
+    // Test Data
+    const order = {
+      orderNumber: 'VLO-JL8DII',
+      color: 'Glacier Blue',
+      wheels: 'aero Wheels',
+      customer: {
+        name: 'Geni Valdo',
+        email: 'genivaldo@velo.dev'
+      },
+      payment_method: 'À Vista',
+      status: 'EM_ANALISE'
+    }
+
+    // Act
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.orderNumber)
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+    // Assert
+    await expect(page.getByTestId(`order-result-${order.orderNumber}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.orderNumber}
+      - img
+      - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment_method}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+    `);
+  })
 
   test('It should display the declined order details after submitting a valid order number', async ({ page }) => {
 
@@ -117,7 +166,6 @@ test.describe('View order', () => {
       - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
     `);
   })
-
 
   test('It should return an error message when submitting an invalid order number', async ({ page }) => {
 
