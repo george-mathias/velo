895deed5f887a7e8fe671ac20a05d26bee047114
George Mathias <gmathias80@gmail.com>
2026-05-02 07:27:39 +0100

    test(e2e): test(e2e): fix OrderLookupPage typo in class name

--- playwright/support/pages/OrderLookupPage.ts
+++ playwright/support/pages/OrderLookupPage.ts
@@ -3,13 +3,63 @@
 // snake_case
 // kebab-case
 
-import { Page } from '@playwright/test'
+import { Page, expect } from '@playwright/test'
+
+type OrderStatus = 'APROVADO' | 'REPROVADO' | 'EM_ANALISE'
 
 export class OrderLookupPage {
-    constructor(private page: Page) {}
-  
-    async buscarPedido(code: string) {
-      await this.page.getByRole('textbox', { name: 'Número do Pedido' }).fill(code)
-      await this.page.getByRole('button', { name: 'Buscar Pedido' }).click()
+    private readonly statusClasses = {
+        APROVADO: ['bg-green-100', 'text-green-700', 'lucide-circle-check-big'],
+        REPROVADO: ['bg-red-100', 'text-red-700', 'lucide-circle-x'],
+        EM_ANALISE: ['bg-amber-100', 'text-amber-700', 'lucide-clock']
+    } as const
+
+    constructor(private page: Page) { }
+
+    async searchOrder(code: string) {
+        await this.page.getByRole('textbox', { name: 'Número do Pedido' }).fill(code)
+        await this.page.getByRole('button', { name: 'Buscar Pedido' }).click()
+    }
+
+    async validateStatusBadge(status: OrderStatus) {
+        const [bgClass, textClass, iconClass] = this.statusClasses[status]
+        const statusBadge = this.page.getByRole('status').filter({ hasText: status })
+
+        await expect(statusBadge).toHaveClass(new RegExp(bgClass))
+        await expect(statusBadge).toHaveClass(new RegExp(textClass))
+        await expect(statusBadge.locator('svg')).toHaveClass(new RegExp(iconClass))
+    }
+
+    async validateOrderResult(order: any) {
+
+        await expect(this.page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - status:
+        - img
+        - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `)
     }
 }
